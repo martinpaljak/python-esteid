17d85b1f321e2ad52443f6a5cc02790244bda46f
Martin Paljak <martin@martinpaljak.net>
2013-04-30 14:52:10 +0300

    Fixups

--- setup.py
+++ setup.py
@@ -1,39 +1,30 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import sys, os
+import sys
+import os
 
-version = '0.1.4'
+setup(
+    name='python-esteid',
+    version='0.1.4',
+    description='Utilities for Estonian eID and mID infrastructure',
+    long_description= open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    classifiers=['Topic :: Utilities',
+                 'Topic :: Security :: Cryptography'],
+    author='Martin Paljak',
+    author_email='martin@martinpaljak.net',
+    url='https://github.com/martinpaljak/python-esteid',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=['M2Crypto>=0.22',
+                      'python-ldap>=2.4.10',
+                      'cliff>=1.3.2'],
+    entry_points={'console_scripts': ['esteid = esteid.main:main'],
+                  'esteid.cli': ['ldap = esteid.cli:LDAP',
+                                 'ssh = esteid.cli:SSH',
+                                 'verify = esteid.cli:Verify',
+                                 'token = esteid.cli:AccessToken']},
+    dependency_links=['http://github.com/martinpaljak/M2Crypto/tarball/master#egg=M2Crypto-0.22'],
+    )
 
-setup(name='python-esteid',
-      version=version,
-      description="Utilities for Estonian eID and mID infrastructure",
-      long_description="""\
-""",
-      classifiers=['Topic :: Utilities', 'Topic :: Security :: Cryptography'],
-      author='Martin Paljak',
-      author_email='martin@martinpaljak.net',
-      url='https://github.com/martinpaljak/python-esteid',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          'suds>=0.4',
-          'M2Crypto>=0.21.1',
-          'python-ldap>=2.4.10',
-          'cliff>=1.3.2'
-      ],
-      entry_points = {
-        'nose.plugins': [
-            'mid_token_plugin = tokenplugin:MidTokenPlugin'
-        ],
-        'console_scripts': [
-          'esteid = esteid.main:main'
-        ],
-        'esteid.cli': [
-          'ldap = esteid.cli:LDAP',
-          'ssh = esteid.cli:SSH',
-          'verify = esteid.cli:Verify',
-          'token = esteid.cli:AccessToken',
-        ],
-       },
-      )
