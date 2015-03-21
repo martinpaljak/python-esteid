c2d5e16328c2a91d50eac0240b37d6fe719b9dbd
Martin Paljak <martin@martinpaljak.net>
2015-03-21 09:48:31 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name='python-esteid',
-    version='0.1.7',
+    version='0.1.8',
     description='Utilities for Estonian eID and mID infrastructure',
     long_description= open(os.path.join(os.path.dirname(__file__), 'README')).read(),
     classifiers=['Topic :: Utilities',
